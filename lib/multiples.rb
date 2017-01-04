8fe09a38ff2a94221064563dd2495a896fad6e31
wongjenn <wongjenn@yahoo.com>
2017-01-04 11:26:18 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,20 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  sum = []
+  (3...limit).each do |count|
+    if count % 3 == 0 || count % 5 == 0
+      sum << count
+    end
+  end
+  sum
+end
+
+def sum_multiples(limit)
+  sum = 0
+  (3...limit).each do |count|
+    if count%3 == 0 || count%5 == 0
+      sum += count
+    end
+  end
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    sum = []
+    (3...limit).each do |count|
+      if count % 3 == 0 || count % 5 == 0
+        sum << count
+      end
+    end
+    sum
+  end
+
+  def sum_multiples
+    sum = 0
+    (3...limit).each do |count|
+      if count%3 == 0 || count%5 == 0
+        sum += count
+      end
+    end
+    sum
+  end
+
+end
